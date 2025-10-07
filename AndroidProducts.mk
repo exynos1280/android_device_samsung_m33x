4302f2b7e0b0e63da606e9bed133f49e9a18c754
Majaahh <maja.blaszczak@proton.me>
2025-10-07 16:35:31 +0200

    pos
    Change-Id: Ic8bb401dff6052ffafb74df7d4730ddc2cf8ffe4
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m33x.mk
+    $(LOCAL_DIR)/aosp_m33x.mk
--- aosp_m33x.mk
+++ aosp_m33x.mk
@@ -18,17 +18,19 @@
 $(call inherit-product, device/samsung/m33x/device.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := m33x
-PRODUCT_NAME := lineage_m33x
+PRODUCT_NAME := aosp_m33x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M336B
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 34
 BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_CHARACTERISTICS := phone
+
+TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="m33xxx-user 15 AP3A.240905.015.A2 M336BXXSDFYG2 release-keys" \
